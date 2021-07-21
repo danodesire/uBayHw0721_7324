18f5fec763d447f89d1f8c502541bfd67ada8ca8
Danodesire <Danodesire@DESKTOP-V490KHT>
2021-07-21 13:32:43 +0800

    Add project files.

---
+++ VehicleLicenseTax/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace VehicleLicenseTax
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxSelect_Usage = new System.Windows.Forms.ComboBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.labelUsage = new System.Windows.Forms.Label();
+            this.labelMotor = new System.Windows.Forms.Label();
+            this.buttonComfirm = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.cbxSelect_Motor = new System.Windows.Forms.ComboBox();
+            this.radio_Allyear = new System.Windows.Forms.RadioButton();
+            this.radioPeriod = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelEnd = new System.Windows.Forms.Label();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.gbDuration = new System.Windows.Forms.GroupBox();
+            this.gbUsageCC = new System.Windows.Forms.GroupBox();
+            this.gpbResult = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.gbDuration.SuspendLayout();
+            this.gbUsageCC.SuspendLayout();
+            this.gpbResult.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbxSelect_Usage
+            // 
+            this.cbxSelect_Usage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxSelect_Usage.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbxSelect_Usage.FormattingEnabled = true;
+            this.cbxSelect_Usage.Items.AddRange(new object[] {
+            "機車",
+            "貨車",
+            "大客車",
+            "自用小客車",
+            "營業用小客車"});
+            this.cbxSelect_Usage.Location = new System.Drawing.Point(348, 35);
+            this.cbxSelect_Usage.Name = "cbxSelect_Usage";
+            this.cbxSelect_Usage.Size = new System.Drawing.Size(598, 41);
+            this.cbxSelect_Usage.TabIndex = 0;
+            this.cbxSelect_Usage.SelectedIndexChanged += new System.EventHandler(this.cbxSelect_Usage_SelectedIndexChanged);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtAmount.Location = new System.Drawing.Point(19, 43);
+            this.txtAmount.Multiline = true;
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.ReadOnly = true;
+            this.txtAmount.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAmount.Size = new System.Drawing.Size(936, 244);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // labelUsage
+            // 
+            this.labelUsage.AutoSize = true;
+            this.labelUsage.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelUsage.Location = new System.Drawing.Point(239, 38);
+            this.labelUsage.Name = "labelUsage";
+            this.labelUsage.Size = new System.Drawing.Size(81, 33);
+            this.labelUsage.TabIndex = 4;
+            this.labelUsage.Text = "用途";
+            this.labelUsage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelMotor
+            // 
+            this.labelMotor.AutoSize = true;
+            this.labelMotor.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelMotor.Location = new System.Drawing.Point(23, 121);
+            this.labelMotor.Name = "labelMotor";
+            this.labelMotor.Size = new System.Drawing.Size(297, 33);
+            this.labelMotor.TabIndex = 5;
+            this.labelMotor.Text = "汽缸CC數/馬達馬力";
+            this.labelMotor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonComfirm
+            // 
+            this.buttonComfirm.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonComfirm.Location = new System.Drawing.Point(974, 88);
+            this.buttonComfirm.Name = "buttonComfirm";
+            this.buttonComfirm.Size = new System.Drawing.Size(140, 49);
+            this.buttonComfirm.TabIndex = 6;
+            this.buttonComfirm.Text = "確認";
+            this.buttonComfirm.UseVisualStyleBackColor = true;
+            this.buttonComfirm.Click += new System.EventHandler(this.buttonComfirm_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonClear.Location = new System.Drawing.Point(974, 184);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(140, 49);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "清除";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // cbxSelect_Motor
+            // 
+            this.cbxSelect_Motor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxSelect_Motor.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbxSelect_Motor.FormattingEnabled = true;
+            this.cbxSelect_Motor.Items.AddRange(new object[] {
+            ""});
+            this.cbxSelect_Motor.Location = new System.Drawing.Point(348, 121);
+            this.cbxSelect_Motor.Name = "cbxSelect_Motor";
+            this.cbxSelect_Motor.Size = new System.Drawing.Size(598, 41);
+            this.cbxSelect_Motor.TabIndex = 9;
+            // 
+            // radio_Allyear
+            // 
+            this.radio_Allyear.AutoSize = true;
+            this.radio_Allyear.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radio_Allyear.Location = new System.Drawing.Point(3, 29);
+            this.radio_Allyear.Name = "radio_Allyear";
+            this.radio_Allyear.Size = new System.Drawing.Size(135, 37);
+            this.radio_Allyear.TabIndex = 11;
+            this.radio_Allyear.TabStop = true;
+            this.radio_Allyear.Text = "全年度";
+            this.radio_Allyear.UseVisualStyleBackColor = true;
+            // 
+            // radioPeriod
+            // 
+            this.radioPeriod.AutoSize = true;
+            this.radioPeriod.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioPeriod.Location = new System.Drawing.Point(3, 99);
+            this.radioPeriod.Name = "radioPeriod";
+            this.radioPeriod.Size = new System.Drawing.Size(135, 37);
+            this.radioPeriod.TabIndex = 12;
+            this.radioPeriod.TabStop = true;
+            this.radioPeriod.Text = "依期間";
+            this.radioPeriod.UseVisualStyleBackColor = true;
+            this.radioPeriod.CheckedChanged += new System.EventHandler(this.radioPeriod_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelEnd);
+            this.panel1.Controls.Add(this.labelStart);
+            this.panel1.Controls.Add(this.dtpEnd);
+            this.panel1.Controls.Add(this.dtpStart);
+            this.panel1.Controls.Add(this.radioPeriod);
+            this.panel1.Controls.Add(this.radio_Allyear);
+            this.panel1.Location = new System.Drawing.Point(182, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(787, 177);
+            this.panel1.TabIndex = 13;
+            // 
+            // labelEnd
+            // 
+            this.labelEnd.AutoSize = true;
+            this.labelEnd.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelEnd.Location = new System.Drawing.Point(286, 125);
+            this.labelEnd.Name = "labelEnd";
+            this.labelEnd.Size = new System.Drawing.Size(48, 33);
+            this.labelEnd.TabIndex = 18;
+            this.labelEnd.Text = "訖";
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelStart.Location = new System.Drawing.Point(286, 72);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(48, 33);
+            this.labelStart.TabIndex = 17;
+            this.labelStart.Text = "起";
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.CalendarFont = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dtpEnd.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dtpEnd.Location = new System.Drawing.Point(366, 116);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(278, 47);
+            this.dtpEnd.TabIndex = 16;
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.CalendarFont = new System.Drawing.Font("PMingLiU", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dtpStart.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dtpStart.Location = new System.Drawing.Point(366, 63);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(278, 47);
+            this.dtpStart.TabIndex = 15;
+            // 
+            // gbDuration
+            // 
+            this.gbDuration.Controls.Add(this.panel1);
+            this.gbDuration.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.gbDuration.Location = new System.Drawing.Point(98, 29);
+            this.gbDuration.Name = "gbDuration";
+            this.gbDuration.Size = new System.Drawing.Size(1009, 212);
+            this.gbDuration.TabIndex = 14;
+            this.gbDuration.TabStop = false;
+            this.gbDuration.Text = "使用期限";
+            // 
+            // gbUsageCC
+            // 
+            this.gbUsageCC.Controls.Add(this.cbxSelect_Motor);
+            this.gbUsageCC.Controls.Add(this.labelMotor);
+            this.gbUsageCC.Controls.Add(this.labelUsage);
+            this.gbUsageCC.Controls.Add(this.cbxSelect_Usage);
+            this.gbUsageCC.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.gbUsageCC.Location = new System.Drawing.Point(98, 259);
+            this.gbUsageCC.Name = "gbUsageCC";
+            this.gbUsageCC.Size = new System.Drawing.Size(1009, 174);
+            this.gbUsageCC.TabIndex = 15;
+            this.gbUsageCC.TabStop = false;
+            this.gbUsageCC.Text = "用途及排氣量";
+            // 
+            // gpbResult
+            // 
+            this.gpbResult.Controls.Add(this.txtAmount);
+            this.gpbResult.Controls.Add(this.buttonClear);
+            this.gpbResult.Controls.Add(this.buttonComfirm);
+            this.gpbResult.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.gpbResult.Location = new System.Drawing.Point(98, 456);
+            this.gpbResult.Name = "gpbResult";
+            this.gpbResult.Size = new System.Drawing.Size(1142, 293);
+            this.gpbResult.TabIndex = 16;
+            this.gpbResult.TabStop = false;
+            this.gpbResult.Text = "試算結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1292, 761);
+            this.Controls.Add(this.gpbResult);
+            this.Controls.Add(this.gbUsageCC);
+            this.Controls.Add(this.gbDuration);
+            this.Name = "Form1";
+            this.Text = "VehicleLicenseTax";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.gbDuration.ResumeLayout(false);
+            this.gbUsageCC.ResumeLayout(false);
+            this.gbUsageCC.PerformLayout();
+            this.gpbResult.ResumeLayout(false);
+            this.gpbResult.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxSelect_Usage;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label labelUsage;
+        private System.Windows.Forms.Label labelMotor;
+        private System.Windows.Forms.Button buttonComfirm;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.ComboBox cbxSelect_Motor;
+        private System.Windows.Forms.RadioButton radio_Allyear;
+        private System.Windows.Forms.RadioButton radioPeriod;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.Label labelEnd;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.GroupBox gbDuration;
+        private System.Windows.Forms.GroupBox gbUsageCC;
+        private System.Windows.Forms.GroupBox gpbResult;
+    }
+}
+
---
+++ VehicleLicenseTax/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehicleLicenseTax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Init();
+        }
+        
+        private void Init() // variable initialization.
+        {
+            this.txtAmount.Text = "";
+            this.cbxSelect_Usage.SelectedIndex = 0;
+            this.cbxSelect_Motor.SelectedIndex = 0;
+            this.dtpStart.Value = DateTime.Now.Date;
+            this.dtpStart.Enabled = false;
+            this.dtpEnd.Value = DateTime.Now.Date;
+            this.dtpEnd.Enabled = false;
+            this.radio_Allyear.Checked = true;
+        }
+
+        private void radioPeriod_CheckedChanged(object sender, EventArgs e)
+        {
+            // setting DateTimePicker disable if RadioButton first selection been chosen.
+            dtpStart.Enabled = !(radio_Allyear.Checked);
+            dtpEnd.Enabled = !(radio_Allyear.Checked);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e) // reset all data.
+        {
+            this.Init();
+        }
+
+        private void cbxSelect_Usage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (this.cbxSelect_Usage.SelectedIndex)
+            {
+                // 機車
+                case 0:
+                    this.cbxSelect_Motor.Items.Clear();
+                    this.cbxSelect_Motor.Items.AddRange(new object[] {
+                    "150以下 / 12HP以下(12.2PS以下)",
+                    "151-250 / 12.1-20HP(12.3-20.3PS)",
+                    "251-500 / 20.1HP以上(20.4PS以上)",
+                    "501-600",
+                    "601-1200",
+                    "1201-1800",
+                    "1801或以上" });
+                    break;
+                // 貨車
+                case 1:
+                    this.cbxSelect_Motor.Items.Clear();
+                    this.cbxSelect_Motor.Items.AddRange(new object[] {
+                    "500以下",
+                    "501-600",
+                    "601-1200",
+                    "1201-1800",
+                    "1801-2400",
+                    "2401-3000 / 138HP以下(140.1PS以下)",
+                    "3001-3600",
+                    "3601-4200 / 138.1-200HP(140.2-203.0PS)",
+                    "4201-4800",
+                    "4801-5400 / 200.1-247HP(203.1-250.7PS)",
+                    "5401-6000",
+                    "6001-6600 / 247.1-286HP(250.8-290.3PS)",
+                    "6601-7200",
+                    "7201-7800 / 286.1-336HP(290.4-341.0PS)",
+                    "7801-8400",
+                    "8401-9000 / 336.1-361HP(341.1-366.4PS)",
+                    "9001-9600",
+                    "9601-10200 / 361.1HP以上(366.5PS以上)",
+                    "10201以上" });
+                    break;
+                // 大客車
+                case 2:
+                    this.cbxSelect_Motor.Items.Clear();
+                    this.cbxSelect_Motor.Items.AddRange(new object[] {
+                    "600以下",
+                    "601-1200",
+                    "1201-1800",
+                    "1801-2400",
+                    "2401-3000 / 138HP以下(140.1PS以下)",
+                    "3001-3600",
+                    "3601-4200 / 138.1-200HP(140.2-203.0PS)",
+                    "4201-4800",
+                    "4801-5400 / 200.1-247HP(203.1-250.7PS)",
+                    "5401-6000",
+                    "6001-6600 / 247.1-286HP(250.8-290.3PS)",
+                    "6601-7200",
+                    "7201-7800 / 286.1-336HP(290.4-341.0PS)",
+                    "7801-8400",
+                    "8401-9000 / 336.1-361HP(341.1-366.4PS)",
+                    "9001-9600",
+                    "9601-10200 / 361.1HP以上(366.5PS以上)",
+                    "10201以上"});
+                    break;
+                // 自用小客車
+                case 3:
+                    this.cbxSelect_Motor.Items.Clear();
+                    this.cbxSelect_Motor.Items.AddRange(new object[] {
+                    "500以下 / 38HP以下(38.6PS以下)",
+                    "501~600 / 38.1-56HP(38.7-56.8PS)",
+                    "601~1200 / 56.1-83HP(56.9-84.2PS)",
+                    "1201~1800 / 83.1-182HP(84.3-184.7PS)",
+                    "1801~2400 / 182.1-262HP(184.8-265.9PS)",
+                    "2401~3000 / 262.1-322HP(266-326.8PS)",
+                    "3001-4200 / 322.1-414HP(326.9-420.2PS",
+                    "4201-5400 / 414.1-469HP(420.3-476.0PS)",
+                    "5401-6600 / 469.1-509HP(476.1-516.6PS)",
+                    "6601-7800 / 509.1HP以上(516.7PS以上)",
+                    "7801以上"});
+                    break;
+                // 營業用小客車
+                case 4:
+                    this.cbxSelect_Motor.Items.Clear();
+                    this.cbxSelect_Motor.Items.AddRange(new object[] {
+                    "500以下 / 38HP以下(38.6PS以下)",
+                    "501~600 / 38.1-56HP(38.7-56.8PS)",
+                    "601~1200 / 56.1-83HP(56.9-84.2PS)",
+                    "1201~1800 / 83.1-182HP(84.3-184.7PS)",
+                    "1801~2400 / 182.1-262HP(184.8-265.9PS)",
+                    "2401~3000 / 262.1-322HP(266-326.8PS)",
+                    "3001-4200 / 322.1-414HP(326.9-420.2PS)",
+                    "4201-5400 / 414.1-469HP(420.3-476.0PS)",
+                    "5401-6600 / 469.1-509HP(476.1-516.6PS)",
+                    "6601-7800 / 509.1HP以上(516.7PS以上)",
+                    "7801以上"});
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private double getPrice() // get price.
+        {
+            double pricePerYear = 0;
+            switch (this.cbxSelect_Usage.SelectedIndex)
+            {
+                case 0:
+                    double[] pScooter = { 0, 800, 1620, 2160, 4320, 7120, 11230 };
+                    pricePerYear = pScooter[this.cbxSelect_Motor.SelectedIndex];
+                    break;
+                case 1:
+                    double[] pTruck = { 900, 1080, 1800, 2700, 3600, 4500, 5400, 6300, 7200, 8100, 9000, 9900, 10800, 11700, 12600, 12600, 13500, 14400, 15300, 15300, 16200 };
+                    pricePerYear = pTruck[this.cbxSelect_Motor.SelectedIndex];
+                    break;
+                case 2:
+                    double[] pBus = { 1080, 1800, 2700, 3600, 4500, 5400, 6300, 7200, 8100, 9000, 9900, 10800, 11700, 12600, 13500, 14400, 15300, 16200 };
+                    pricePerYear = pBus[this.cbxSelect_Motor.SelectedIndex];
+                    break;
+                case 3:
+                    double[] pSelfCar = { 1620, 2160, 4320, 7120, 11230, 15210, 28220, 46170, 69690, 117000, 151200 };
+                    pricePerYear = pSelfCar[this.cbxSelect_Motor.SelectedIndex];
+                    break;
+                case 4:
+                    double[] pBusiness = { 900, 1260, 2160, 3060, 6480, 9900, 16380, 24300, 33660, 44460, 56700 };
+                    pricePerYear = pBusiness[this.cbxSelect_Motor.SelectedIndex];
+                    break;
+                default:
+                    break;
+            }
+            return pricePerYear;
+        }
+
+        private double getDays() // get total days.
+        {
+            DateTime startDate = this.dtpStart.Value;
+            DateTime endDate = this.dtpEnd.Value;
+            double totalDays = (endDate - startDate).TotalDays + 1; // counting way : start day 00:00 ~ end day 24:00
+            return totalDays;
+        }
+
+        private bool checkDaysAmount() // check days amount is correct or not.
+        {
+            if (this.getDays() < 0)
+            {
+                this.Init();
+                this.txtAmount.Text = "Please check about the date duration.";
+                return false;
+            }
+            return true;
+        }
+
+        private void printResult()
+        {
+            // leap year check.
+            DateTime leapYearCheck = DateTime.Now;
+            double dayAmount = 0;
+            double yearDays = 0;
+            double taxResult = 0;
+
+            if (DateTime.IsLeapYear(leapYearCheck.Year))
+            {
+                yearDays = 366;
+                dayAmount = 366;
+            }                
+            else
+            {
+                yearDays = 365;
+                dayAmount = 365;
+            }                             
+
+            if (this.radio_Allyear.Checked == true)
+            {
+                this.txtAmount.Text = $"使用期間:  {leapYearCheck.Year}/1/1 上午 12:00:00 ~ {leapYearCheck.Year}/12/31 上午 12:00:00\r\n";
+                this.txtAmount.Text += $"計算天數:  {dayAmount}\r\n";
+                this.txtAmount.Text += $"汽缸CC數: {this.cbxSelect_Motor.Text}\r\n";
+                this.txtAmount.Text += $"用途:         {this.cbxSelect_Usage.Text}\r\n";
+
+                taxResult = Math.Floor(getPrice() * dayAmount / yearDays);
+                this.txtAmount.Text += $"計算公式:  {getPrice()} * {dayAmount} / {yearDays} = {taxResult}元\r\n";
+                this.txtAmount.Text += $"應納稅額:  共{taxResult}元";
+            }
+            else
+            {
+                DateTime startDate = this.dtpStart.Value;
+                this.txtAmount.Text = "";
+                int loopTest = 0;
+                while (true)
+                {
+                    leapYearCheck = startDate;
+                    if (DateTime.IsLeapYear(leapYearCheck.Year))
+                        yearDays = 366;
+                    else
+                        yearDays = 365;
+
+                    DateTime yearEndDate = new DateTime(startDate.Year, 12, 31);
+                    if (this.dtpEnd.Value <  yearEndDate)
+                        yearEndDate = this.dtpEnd.Value;
+
+                    double daysTemp = (yearEndDate - startDate).TotalDays + 1;
+
+                    this.txtAmount.Text += $"使用期間:  {startDate} ~ {yearEndDate}\r\n";
+                    this.txtAmount.Text += $"計算天數:  {daysTemp}\r\n";
+                    this.txtAmount.Text += $"汽缸CC數: {this.cbxSelect_Motor.Text}\r\n";
+                    this.txtAmount.Text += $"用途:         {this.cbxSelect_Usage.Text}\r\n";
+
+                    double taxResultTemp = Math.Floor(getPrice() * daysTemp / yearDays);
+                    taxResult += taxResultTemp;
+                    this.txtAmount.Text += $"計算公式:  {getPrice()} * {daysTemp} / {yearDays} = {taxResultTemp}元\r\n";
+                    this.txtAmount.Text += $"應納稅額:  共{taxResultTemp}元\r\n\r\n";
+                    
+                    loopTest += 1;
+                    if (yearEndDate == this.dtpEnd.Value)
+                        break;                      
+                    else
+                        startDate = new DateTime(startDate.Year + 1, 1, 1);                     
+                }
+                if(loopTest > 1)
+                    this.txtAmount.Text += $"全部應納稅額:  共{taxResult}元\r\n";
+            }
+        }
+
+        private void buttonComfirm_Click(object sender, EventArgs e)
+        {
+            if(this.checkDaysAmount())
+            {
+                if (this.cbxSelect_Motor.SelectedIndex > -1 && this.cbxSelect_Usage.SelectedIndex > -1)
+                    this.printResult();
+                else
+                    this.txtAmount.Text = "Please select usage or CC number.";
+            }
+        }
+    }
+}
